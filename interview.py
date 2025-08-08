89251feee6478a9647e620786c390a89fe705e22
WAN LI <ygwanli25@gmail.com>
2025-08-08 13:53:43 +0800

    initial commit

---
+++ interview.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+name = input("Enter your name: ")
+
+
+def get_Time(dt_object):
+    
+
+    if dt_object.hour < 12:
+        return "Morning"
+    else:
+        return "Afternoon"
+    
+    
+date_time = datetime.now()
+
+day = get_Time(date_time)
+morning_time = datetime(2025, 8, 8, 14, 30, 0)
+
+print(f"Hello {name} {get_Time(morning_time)}.")
